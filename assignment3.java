87794670155b77b511234bfdd4d428d96de80494
dalbergiaretusa <bastianksiazczyk@gmail.com>
2017-01-18 14:05:44 +0000

    skopiowalem od tymona

--- assignment3.java
+++ assignment3.java
@@ -10,43 +10,84 @@ import sheffield.*;
  * @author sdn
  */
 
-public class Assignment3 {
+public class assignment3 {
 
     /**
      * @param args the command line arguments
      */
-    //robot goes forward until it finds a black line
-    /*public static findTheLine() {
-
-    }*/
-    //robot follows the black line (ideally to the left) until it finds a coloured dot
+    /*
+      robot goes forward until it finds a black line
+      it goes straightforward until black  or brown is seen - then it rotate to the left until it sees the line again.
+    */
     boolean used[] = new boolean[9];
     int posX[] = new int[9];
-    int posY[] = new int[9];    
+    int posY[] = new int[9];
+    public static void findTheLine(Robot myRobot, Motor leftMotor, Motor rightMotor, Speaker speaker, ColorSensor colorSensor) {
 
-    public static followTheLine(leftMotor, rightMotor, speaker, colorSensor) {
-        leftMotor.setSpeed(300);
-        rightMotor.setSpeed(300);
+        ColorSensor.Color x;
+
+      //robot follows the black line (ideally to the left) until it finds a coloured dot
+
+      leftMotor.forward();
+      rightMotor.forward();
+      x = colorSensor.getColor();
+
+      while(x != ColorSensor.Color.BLACK && x!=ColorSensor.Color.BROWN) {
+        System.out.println(x);
+        leftMotor.stop();
+        rightMotor.stop();
+        leftMotor.setSpeed(100);
+        rightMotor.setSpeed(100);
         leftMotor.forward();
         rightMotor.forward();
+        myRobot.sleep(100);
 
-        final float TRESHOLD = 0.4; //thershold between black and white
+        x = colorSensor.getColor();
+      }
 
-        while(true) {
-            if(colorSensor.getAmbient() < TRESHOLD) {
-              leftMotor.setSpeed(300);
+      leftMotor.setSpeed(0);
+      rightMotor.setSpeed(80);
+      do {
+        x = colorSensor.getColor();
+      }
+      while(x!=ColorSensor.Color.BLACK);
+
+      return;
+
+    }
+
+    /*
+      robot follows the black line (ideally to the left) until it finds a coloured dot
+      it goes by the right edge of the dot.\
+      TODO: add next behaviour after the dot is found.
+    */
+    public static void followTheLine(Robot myRobot, Motor leftMotor, Motor rightMotor, Speaker speaker, ColorSensor colorSensor) {
+        ColorSensor.Color x;
+        int i =0;
+        final double TRESHOLD = 0.4; //thershold between black and white
+        while(i<300) {
+          leftMotor.forward();
+          rightMotor.forward();
+          x = colorSensor.getColor();
+          System.out.println(x);
+            if(x == ColorSensor.Color.BLACK) {
+              leftMotor.setSpeed(100);
               rightMotor.setSpeed(0);
+
             }
             else {
               leftMotor.setSpeed(0);
-              rightMotor.setSpeed(300);
+              rightMotor.setSpeed(100);
             }
+
+            i++;
         }
-        return false;
+        return;
+
     }
     // robot stores the dot position (how?) and its colour, updates the graph and the boolean array whether this colour has already been used. then checks if the dot has been used before - if so, then calls goToDot(previous dot). otherwise searches for another black line. I could try and implement that and goToDot(seb)
-    public static analyzeDot(colorSensor, positionX, positionY) {
-      ColorSensor.Color dotColor = new ColorSensor.Color;
+    public static void analyzeDot(ColorSensor colorSensor,int positionX,int positionY) {
+      ColorSensor.Color dotColor =  colorSensor.getColor();
 
       if(colorSensor.getColor()!=color.BLACK && colorSensor.getColor()!=color.WHITE) {
         dotColor=colorSensor.getColor();
@@ -60,14 +101,16 @@ public class Assignment3 {
         }
 
       }
+      return;
     }
-    public static goToDot(colorSensor, leftMotor, rightMotor, dotColor) {
-      
+    /*public static goToDot(colorSensor, leftMotor, rightMotor, dotColor) {
     }
     public static void makeSound() {
-
+      return;
     }*/
-    public static void spin(leftMotor, rightMotor) {
+
+
+    public static void spin(Motor leftMotor, Motor rightMotor) {
       leftMotor.setSpeed(300);
       rightMotor.setSpeed(300);
       leftMotor.forward();
@@ -76,10 +119,11 @@ public class Assignment3 {
       long finishTime = curTime+5000;
       if(System.currentTimeMillis() == finishTime) {
         leftMotor.setSpeed(0);
-        rightMotor.setSpeed(0);  
+        rightMotor.setSpeed(0);
       }
     }
-    public static void testFunctions(leftMotor, rightMotor, speaker, colorSensor) {
+
+    public static void testFunctions(Robot myRobot, Motor leftMotor,Motor rightMotor, Speaker speaker, ColorSensor colorSensor ) {
       //test if all the things work
       leftMotor.setSpeed(400);
       rightMotor.setSpeed(400);
@@ -91,21 +135,21 @@ public class Assignment3 {
 
       myRobot.sleep(100);
 
-      return false;
+      return ;
     }
 
     public static void main(String[] args) {
 
         Robot myRobot = new Robot();
-        Sensor mySensor= new Sensor();
 
-        Motor leftMotor = myRobot.getLargeMotor(Motor.Port.B);
-        Motor rightMotor = myRobot.getLargeMotor(Motor.Port.C);
+        ColorSensor colorSensor = myRobot.getColorSensor(Sensor.Port.S1);
+        Motor leftMotor = myRobot.getLargeMotor(Motor.Port.C);
+        Motor rightMotor = myRobot.getLargeMotor(Motor.Port.B);
+
         Speaker speaker = myRobot.getSpeaker();
-        ColorSensor colorSensor = myRobot.getColorSensor(Sensor.Port.S1); //not sure about the sensor.port thing
-        testFunctions(leftMotor, rightMotor, speaker, colorSensor);
-
-        ColorSensor.Color color;
+        //testFunctions(myRobot, leftMotor, rightMotor, speaker, colorSensor);
+        findTheLine(myRobot, leftMotor, rightMotor, speaker, colorSensor);
+        followTheLine(myRobot, leftMotor, rightMotor, speaker, colorSensor);
 
 
 
